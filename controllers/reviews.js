6b11619e573e324c7230f5dac778a53373cfeaf3
Avishaan <avishaan@codeHatcher.com>
2014-11-12 23:09:17 -0800

    add review controller

---
+++ controllers/reviews.js
@@ -0,0 +1,18 @@
+var querystring = require('querystring');
+var logger = require('./../loggers/logger.js');
+var config = require('../config.js');
+
+module.exports.readReview = function readReview (req, res, next) {
+  var rid = req.swagger.params.rid.value;
+  logger.info('Lookup user ' + rid);
+  // make sure to unescape any characters
+  rid = querystring.unescape(rid);
+  // get the user measurements
+  User.getMeasurements({phone: rid}, function(err, measurements){
+    if (!err){
+      res.status(200).send(measurements);
+    } else {
+      res.status(500).send(err);
+    }
+  });
+};
