315518b53599bfbb075349c04b75b5355c9a4446
Avishaan <avishaan@codeHatcher.com>
2014-11-20 22:07:53 -0800

    correct route name

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ var logger = require('./../loggers/logger.js');
 var User = require('./../models/user.js');
 var config = require('../config.js');
 
-module.exports.receiveSMS = function receiveSMS (req, res, next) {
+module.exports.registerUser = function registerUser (req, res, next) {
   logger.info('registerUser');
   res.status(200).send('ok');
 };
