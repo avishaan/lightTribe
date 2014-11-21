7b14ec03c8853d64c9c3b79aa50816e313b84b65
Avishaan <avishaan@codeHatcher.com>
2014-11-20 21:49:35 -0800

    specs for user testing

---
+++ specs/integration/userSpec.js
@@ -0,0 +1,32 @@
+var agent = require('superagent');
+var config = require("../../config.js");
+var fixture = require('./../fixtures/fixture.js');
+var User = require('../../models/user.js');
+//var httpMocks = require('node-mocks-http');
+
+var apiVersion = '/v1';
+var URL = config.apiURI + ':' + config.expressPort + "/api" + apiVersion;
+
+describe("A user", function() {
+  // delete the database before each time
+  beforeEach(function(done){
+    fixture.deleteDB(function(err, user){
+      // make sure it was able to delete the database ok
+      expect(err).toEqual(null);
+      // then seed the db with a user
+    });
+  });
+  it("should be able to register", function(done) {
+    agent
+    .post(URL + '/users')
+    //.get('http://localhost:3000/api/v1/templates')
+    .set('Content-Type', 'application/json')
+    .send({
+      username: 'testUser',
+      password: 'testPassword',
+    })
+    .end(function(res){
+      expect(res.status).toEqual(200);
+    });
+  });
+});
