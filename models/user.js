4b9eca28bfb1ca875f954c16c461160412971867
Avishaan <avishaan@codeHatcher.com>
2014-11-24 17:35:42 -0800

    password hashing added

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var logger = require('./../loggers/logger.js');
+var bcrypt = require('bcrypt');
 /*
 |-------------------------------------------------------------
 | User Schema
@@ -7,8 +8,23 @@ var logger = require('./../loggers/logger.js');
 */
 
 var userSchema = new mongoose.Schema({
-  username: { type: String },
-  password: { type: String },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// if password is modified, re-hash on save
+userSchema.pre('save', function(next) {
+  if (this.isModified('password')){
+    this.hashPassword(function(err, hash){
+      if (err) {
+        return next(err);
+      } else {
+        return next();
+      }
+    });
+  } else {
+    return next();
+  }
 });
 /**
  * Create a new user
@@ -71,6 +87,33 @@ userSchema.statics.checkAuthentication = function(options, cb) {
     }
   });
   // if user exists check the password
+};
+/**
+ * Hash password
+ * @param {function} cb
+ * @config {object} err Passed Error
+ * @config {boolean} hashed password 
+ */
+userSchema.methods.hashPassword = function(cb) {
+  var user = this;
+  var SALT_WORK_FACTOR = 9;
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) {
+      return cb(err);
+    } else {
+      // hash the password with our salt
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) {
+          return cb(err);
+        } else {
+          // override the password with the hashed version
+          user.password = hash;
+          return cb(null, hash);
+        }
+      });
+    }
+  });
 };
 /**
  * Compare two passwords for a match
