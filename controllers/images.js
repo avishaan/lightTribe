3b2072ebf07e5a4426b2081c49fb804931503364
Avishaan <avishaan@codeHatcher.com>
2014-12-05 17:05:52 +0530

    basic uploading

--- controllers/images.js
+++ controllers/images.js
@@ -4,7 +4,12 @@ var config = require('../config.js');
 
 module.exports.createImage = function createImage (req, res, next) {
   logger.info('file created');
-  res.status(200).send('ok');
+  if (req.files && req.files.file && req.files.file.size) {
+    // we have a file, upload it to cloudinary
+    res.status(200).send('ok');
+  } else {
+    res.status(400).send({clientMsg: "No file found"});
+  }
 };
 module.exports.readImage = function readImage (req, res, next) {
   logger.info('file read');
