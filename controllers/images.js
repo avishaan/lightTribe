18f5dd344040bcc4326c8c9a11bad3486da8ccf7
Avishaan <avishaan@codeHatcher.com>
2014-12-05 21:35:51 +0530

    pass test cases for image uploading

--- controllers/images.js
+++ controllers/images.js
@@ -1,12 +1,21 @@
 var logger = require('./../loggers/logger.js');
 var User = require('./../models/user.js');
 var config = require('../config.js');
+var fs = require('fs');
+var cloudinary = require('cloudinary');
+// config cloudinary
+cloudinary.config(config.cloudinary);
 
 module.exports.createImage = function createImage (req, res, next) {
   logger.info('file created');
   if (req.files && req.files.file && req.files.file.size) {
     // we have a file, upload it to cloudinary
-    res.status(200).send('ok');
+    // TODO upload as stream to save space and increase speed
+    cloudinary.uploader.upload(req.files.file.path, function(result){
+      res.status(200).send({
+        _id: result.public_id
+      });
+    });
   } else {
     res.status(400).send({clientMsg: "No file found"});
   }
