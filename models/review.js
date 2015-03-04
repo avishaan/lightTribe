f90f8894d6dcbe98979bc52cb6a893dc2d32d19c
Avishaan <avishaan@codeHatcher.com>
2015-03-04 17:46:00 +0800

    implement: basic implementation of #15

--- models/review.js
+++ models/review.js
@@ -1,34 +1,35 @@
 var mongoose = require('mongoose');
 var logger = require('./../loggers/logger.js');
+var cloudinary = require('cloudinary');
+var User = require('./../models/user.js');
+var config = require('../config.js');
+var async = require('async');
+
+// config cloudinary
+cloudinary.config(config.cloudinary);
 /*
 |-------------------------------------------------------------
-| Review Schema
+| Image Schema
 |-------------------------------------------------------------
 */
 
-var reviewSchema = new mongoose.Schema({
-  company: { type: String },
-  description: { type: String },
-  rating: { type: Number },
-  datetime: { type: Date },
-  location: { type: String },
-  images: [
-    { type: String }
-  ],
-  submitter: { type: String },
+var imageSchema = new mongoose.Schema({
+  url: { type: String },
+  hosting: { type: String, default: 'cloudinary' },
+  public_id: { type: String }
 });
 
 /**
- * Create a specific review
- * @param {object} options The options for the new review
- * @config {string} company of the review
+ * Create a specific image
+ * @param {object} options The options for the new image
+ * @config {string} company of the image
  * @param {function} cb
- * @config {object} review 
+ * @config {object} image 
  * @config {object} err Passed Error
  */
-reviewSchema.statics.createReview = function(options, cb) {
+imageSchema.statics.createImage = function(options, cb) {
   // we are redefining the object to make sure other random stuff doesn't come through
-  var review = {
+  var image = {
     company: options.company,
     description: options.description,
     rating: options.rating,
@@ -37,32 +38,53 @@ reviewSchema.statics.createReview = function(options, cb) {
     location: options.location,
     submitter: options.submitter
   };
-
-  // add review to the database
-  Review.create(review, function(err, savedReview){
-    if (!err && savedReview){
-      // we created the savedReview successfully
-      cb(null, savedReview);
+  async.waterfall([
+  function(done){
+    //upload the image, if success then add to own database
+    cloudinary.uploader.upload(req.files.file.path, function(result){
+      if (!result.error){
+        done(null, result.public_id);
+      } else {
+        done(result);
+      }
+    });
+  },
+  function(public_id, done){
+    // add image to the database
+    Image.create({ public_id: public_id }, function(err, savedImage){
+      if (!err && savedImage){
+        // we created the savedImage successfully
+        done(null, savedImage);
+      } else {
+        done({err: err, clientMsg: 'Something broke, try again'}, null);
+      }
+    });
+  }
+  ], function(err, result){
+    if (!err){
+      cb(null, result);
     } else {
       logger.error(err);
-      cb({err: err, clientMsg: 'Something broke, try again'}, null);
+      cb(err);
     }
   });
+
+
 };
 /**
- * Read a specific review
+ * Read a specific image
  * @param {object} options The options for the lookup
- * @config {string} review id of the review
+ * @config {string} image id of the image
  * @param {function} cb
- * @config {object} review 
+ * @config {object} image 
  * @config {object} err Passed Error
  */
-reviewSchema.statics.readReview = function(options, cb) {
+imageSchema.statics.readImage = function(options, cb) {
   var id = options.id;
-  // see if review exists, if so pass error
-  Review.findOne({_id: id}, function(err, review){
+  // see if image exists, if so pass error
+  Image.findOne({_id: id}, function(err, image){
     if (!err){
-      cb(null, review);
+      cb(null, image);
     } else {
       // we had some sort of database error
       logger.error(err);
@@ -71,7 +93,7 @@ reviewSchema.statics.readReview = function(options, cb) {
   });
 };
 
-var Review = mongoose.model('Review', reviewSchema);
+var Image = mongoose.model('Image', imageSchema);
 
-module.exports = Review;
+module.exports = Image;
 
