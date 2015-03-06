88bb390afb424ce0bb3d8a77ed4e9505ccc0171c
Avishaan <avishaan@codeHatcher.com>
2015-03-06 22:15:12 +0800

    create review using promises

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,16 +3,20 @@ var logger = require('./../loggers/logger.js');
 var config = require('../config.js');
 var Review = require('../models/review.js');
 
+var Promise = require('bluebird');
+Promise.promisifyAll(Review);
+
 module.exports.createReview = function createReview (req, res, next) {
   var review = req.swagger.params.review.value;
   // get the userid from the authenticated user, they are the one that submitted
   review.submitter = req.user.id;
-  Review.createReview(review, function(err, review){
-    if (!err){
-      res.status(200).send(review);
-    } else {
-      res.status(500).send(err);
-    }
+  Review
+  .createReviewAsync(review)
+  .then(function(review){
+    res.status(200).send(review);
+  })
+  .catch(function(err){
+    res.status(500).send(err);
   });
 };
 
