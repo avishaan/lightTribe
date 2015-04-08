544d720b90eeca8e482222a5977aa54bcb14b24c
Avishaan <avishaan@codeHatcher.com>
2015-04-08 00:34:08 -0400

    implement: when image population not successful don't return review at all

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,5 +1,6 @@
 var logger = require('./../loggers/logger.js');
 var Review = require('../models/review.js');
+var async = require('async');
 
 var Promise = require('bluebird');
 Promise.promisifyAll(Review);
@@ -33,13 +34,45 @@ module.exports.readReview = function readReview (req, res, next) {
 
 module.exports.readAllReviews = function readReviews (req, res, next) {
   logger.info('Lookup All Reviews for user: ' + req.user.id);
-  // get the Review
-  Review.readAllReviews({userId: req.user.id}, function(err, reviews){
-    if (!err){
-      reviews = reviews.map(function(review){
-        return review.toObject({ transform: hideProperties });
+  var allReviews;
+  var reviewsWithImage= [];
+
+  async.series([
+  function(cb){
+    // get the Reviews
+    Review.readAllReviews({userId: req.user.id}, function(err, reviews){
+      if (!err){
+        allReviews = reviews;
+        cb(null);
+      } else {
+        cb(err);
+      }
+    });
+  },
+  function(cb){
+    //populate the images for the reviews
+    async.each(allReviews, function(review, callback){
+      Review.populate(review, {path: 'images'}, function(err, review){
+        if (!err) {
+          reviewsWithImage.push(review);
+          callback(null);
+        } else {
+          callback(null);
+        }
       });
-      res.status(200).send(reviews);
+    }, function(err){
+      cb(err);
+    });
+  },
+  function(cb){
+    allReviews = reviewsWithImage.map(function(review){
+      return review.toObject({ transform: hideProperties });
+    });
+    cb(null, allReviews);
+  }
+  ], function(err, results){
+    if (!err){
+      res.status(200).send(allReviews);
     } else {
       res.status(500).send(err);
     }
