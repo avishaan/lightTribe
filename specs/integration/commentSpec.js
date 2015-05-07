523f2c1b1ffb58dd7099027c33426f698f9bf396
Avishaan <avishaan@codeHatcher.com>
2015-05-07 02:21:14 -0700

    test: add spec for comment retrieval #7

--- specs/integration/commentSpec.js
+++ specs/integration/commentSpec.js
@@ -9,13 +9,14 @@ var URL = config.apiURI + ':' + config.expressPort + "/api" + apiVersion;
 
 // complete post for testing
 var post = {
+  _id: "12345", //TODO: remove this fake post
   text: 'This is a post description',
   images: ['uhn43civzs6m1c9uurqvr', 'uhn43civzs6m1c9uurqvj', 'uhn43civzs6m1c9uurqvo'],
   latitude: '1234.5',
-  logitude: '1234.5'
+  longitude: '1234.5'
 };
 
-describe("Creating a post", function() {
+describe("Comments", function() {
   // delete the database before each time
   beforeEach(function(done){
     fixture.deleteDB(function(err, user){
@@ -26,136 +27,43 @@ describe("Creating a post", function() {
         // save the user for later
         seedUser = user;
         expect(err).toEqual(null);
-        // create a post that we will use with the comments
-        agent
-        .post(URL + '/posts')
-        .set('Content-Type', 'application/json')
-        .send(post)
-        .send({ access_token: seedUser.token })
-        .end(function(res){
-          expect(res.status).toEqual(200);
-          done();
-        });
-      });
-    });
-  });
-  it("should require access_token to be filled out", function(done) {
-    agent
-    .post(URL + '/posts')
-    //.get('http://localhost:3000/api/v1/templates')
-    .set('Content-Type', 'application/json')
-    .send(post)
-    .end(function(res){
-      expect(res.status).toEqual(401);
-      done();
-    });
-  });
-  it("should require a valid authentication token to access", function(done) {
-    agent
-    .post(URL + '/posts')
-    //.get('http://localhost:3000/api/v1/templates')
-    .set('Content-Type', 'application/json')
-    .send(post)
-    .send({ access_token: 'wrongtoken' })
-    .end(function(res){
-      expect(res.status).toEqual(401);
-      done();
-    });
-  });
-  it("should be able to be submitted successfully", function(done) {
-    agent
-    .post(URL + '/posts')
-    .set('Content-Type', 'application/json')
-    .send(post)
-    .send({ access_token: seedUser.token })
-    .end(function(res){
-      var body = res.body;
-      expect(res.status).toEqual(200);
-      expect(body._id).toBeDefined();
-      expect(body.text).toEqual(post.text);
-      expect(body.images).toEqual(post.images);
-      expect(body.latitude).toEqual(post.latitude);
-      expect(body.logitude).toEqual(post.logitude);
-      expect(body.submitter).toEqual(seedUser.id);
-      done();
-    });
-  });
-  it("should require a text field to be filled out", function(done) {
-    agent
-    .post(URL + '/posts')
-    .set('Content-Type', 'application/json')
-    .send({ access_token: seedUser.token })
-    .send({
-      images: post.images,
-      latitude: post.latitude,
-      logitude: post.logitude
-    })
-    .end(function(res){
-      // TODO need specific error message describing what is missing
-      expect(res.status).toEqual(400);
-      done();
-    });
-  });
-});
-
-//TODO remove this for production
-xdescribe("Posts", function() {
-  // delete the database before each time
-  beforeEach(function(done){
-    fixture.deleteDB(function(err, user){
-      // make sure it was able to delete the database ok
-      expect(err).toEqual(null);
-      // seed a user
-      fixture.seedUser(function(err, user){
-        // save the user for later
-        seedUser = user;
-        expect(err).toEqual(null);
-        // seed review
-        fixture.seedReview({user: seedUser},function(err, review){
-          expect(err).toEqual(null);
-          done();
-        });
-      });
-    });
-  });
-  it("should be retrievable in list form by a user", function(done) {
-    agent
-    .get(URL + '/posts')
-    //.get('http://localhost:3000/api/v1/templates')
-    .set('Content-Type', 'application/json')
-    .query({access_token: seedUser.token})
-    .end(function(res){
-      var posts = res.body;
-      expect(posts.length).toEqual(1);
-      expect(posts[0]._id).toBeDefined();
-      expect(res.status).toEqual(200);
-      done();
-    });
-  });
-  it("should be retrievable even when imageid in post is not real", function(done) {
-    // post a post with fake images
-    agent
-    .post(URL + '/posts')
-    .set('Content-Type', 'application/json')
-    .send(post)
-    .send({ access_token: seedUser.token })
-    .end(function(res){
-      var body = res.body;
-      agent
-      .get(URL + '/posts')
-      //.get('http://localhost:3000/api/v1/templates')
-      .set('Content-Type', 'application/json')
-      .query({access_token: seedUser.token})
-      .end(function(res){
-        var posts = res.body;
-        expect(res.status).toEqual(200);
-        expect(posts.length).toEqual(2);
-        // make sure one of them has images populated
-        expect(posts[0].images.length || posts[1].images.length).toBeTruthy();
-        // make sure one of them doesn't have images populated (yes, not a great test)
-        expect(posts[0].images.length && posts[1].images.length).toBeFalsy();
         done();
       });
+    });
+  });
+  //it("should require access_token to be filled out", function(done) {
+  //  agent
+  //  .get(URL + '/profile/' + seedUser.id)
+  //  //.get('http://localhost:3000/api/v1/templates')
+  //  .set('Content-Type', 'application/json')
+  //  .send(post)
+  //  .end(function(res){
+  //    expect(res.status).toEqual(401);
+  //    done();
+  //  });
+  //});
+  //it("should require a valid authentication token to access", function(done) {
+  //  agent
+  //  .get(URL + '/profile/' + seedUser.id)
+  //  //.get('http://localhost:3000/api/v1/templates')
+  //  .set('Content-Type', 'application/json')
+  //  .send(post)
+  //  .send({ access_token: 'wrongtoken' })
+  //  .end(function(res){
+  //    expect(res.status).toEqual(401);
+  //    done();
+  //  });
+  //});
+  it("should return all the comments for a post", function(done) {
+    agent
+    .get(URL + '/posts/' + post._id + '/comments')
+    .set('Content-Type', 'application/json')
+    .query({ access_token: seedUser.token })
+    .end(function(res){
+      var comments = res.body;
+      expect(comments.length).toBeDefined();
+      expect(res.status).toEqual(200);
+      done();
     });
   });
 });
