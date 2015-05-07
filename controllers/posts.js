2e97f1f68cc08469f8553f002dea05b317013315
Avishaan <avishaan@codeHatcher.com>
2015-05-06 19:17:30 -0700

    develop: clean up extra post functions

--- controllers/posts.js
+++ controllers/posts.js
@@ -27,33 +27,6 @@ module.exports.createPost = function (req, res, next) {
   // });
 };
 
-module.exports.createReview = function createReview (req, res, next) {
-  var review = req.swagger.params.review.value;
-  // get the userid from the authenticated user, they are the one that submitted
-  review.submitter = req.user.id;
-  Review
-  .createReviewAsync(review)
-  .then(function(review){
-    res.status(200).send(review);
-  })
-  .catch(function(err){
-    res.status(500).send(err);
-  });
-};
-
-module.exports.readReview = function readReview (req, res, next) {
-  var rid = req.swagger.params.rid.value;
-  logger.info('Lookup Review ' + rid);
-  // get the Review
-  Review.readReview({id: rid}, function(err, review){
-    if (!err){
-      res.status(200).send(review);
-    } else {
-      res.status(500).send(err);
-    }
-  });
-};
-
 module.exports.readAllPosts = function readReviews (req, res, next) {
   logger.info('Lookup All posts relavent to user: ' + req.user.id);
   res.status(200).send([
