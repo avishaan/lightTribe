f5b1cc3fce4c0a83845a443cf5706d3177dfeba0
Avishaan <avishaan@codeHatcher.com>
2015-05-12 01:20:19 -0700

    dev: allow CORS OPTIONS verb

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Expose-Headers", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   next();
 });
 
