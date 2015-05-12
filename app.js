278235514e7109f879ff7675d6ba536d79310edf
Avishaan <avishaan@codeHatcher.com>
2015-05-11 23:38:53 -0700

    dev: allow all headers to be passed through

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var prettyjson = require('prettyjson');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
