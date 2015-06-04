e2b5d36090c98da4613ee6d19cb5e021c8fdb79c
Avishaan <avishaan@codeHatcher.com>
2015-06-03 18:28:50 -0700

    dev: implement search and creation based on GeoJSON geometries for both point to point and multipoint to point. Multipoint is used as an estimate of a point but using radius of 1 mile. Future version should allow point to point to decrease query complexity #25 #27

--- models/post.js
+++ models/post.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var logger = require('./../loggers/logger.js');
+var utils = require('./../utils/circleToPolygon.js');
 /*
 |-------------------------------------------------------------
 | Post Schema
@@ -11,15 +12,17 @@ var postSchema = new mongoose.Schema({
   createDate: { type: Date },
   loc: { // based on GeoJSON
     type: {
-      type: String, default: "Point"
+      type: String, default: "MultiPoint"
     },
-    coordinates: [ Number ]
+    coordinates: []
   },
   images: [
     { type: String, ref: 'Image' }
   ],
   author: { type: String, ref: 'User' },
 });
+
+postSchema.index({loc: '2dsphere'});
 
 /**
  * Create a specific post
@@ -30,18 +33,24 @@ var postSchema = new mongoose.Schema({
  * @config {object} err Passed Error
  */
 postSchema.statics.createPost = function(options, cb) {
+  // convert coordinates to polygon so that we can cover an area
+  var polyCoordinates = utils.circleToPolygon({
+    latitude: options.latitude,
+    longitude: options.longitude,
+    radius: 1, // 1km
+    sides: 12
+  });
   // we are redefining the object to make sure other random stuff doesn't come through
   var post = {
     text: options.text,
     createDate: Date.now(),
     author: options.author,
     images: options.images,
-    geometry: {
-      type: "Point",
-      coordinates:[options.longitude, options.latitude]
-    },
+    loc: {
+      type: "MultiPoint",
+      coordinates: polyCoordinates
+    }
   };
-
   // add post to the database
   Post.create(post, function(err, savedPost){
     if (!err && savedPost){
@@ -52,6 +61,9 @@ postSchema.statics.createPost = function(options, cb) {
       cb(err);
     }
   });
+
+  //helper function takes point and radius and converts to GeoJSON compatible polygon that roughly approximates a circle
+
 };
 /**
  * Read a specific post
@@ -75,6 +87,40 @@ postSchema.statics.readPost = function(options, cb) {
   });
 };
 
+/**
+ * Read relevant post based on query parameters search
+ * @param {object} options The options for the lookup
+ * @config {number} options.longitude longitude of the search for posts in that area using WGS84 guidelines 
+ * @config {number} options.latitude latitude of the search for posts in that area using WGS84 guidelines
+ * @config {number} options.distance in units of meters assuming geoJSON guidelines
+ * @param {function} cb
+ * @config {object} posts
+ * @config {object} err Passed Error
+ */
+postSchema.statics.readPostsBySearch = function(options, cb) {
+  // see if post exists, if so pass error
+  var coordinates = [options.longitude, options.latitude];
+  var distance = options.distance; // km
+  var earthRadius = 6371; // km
+  Post
+  .find({})
+  .where('loc')
+  .near({
+    center: coordinates,
+    spherical: true,
+    maxDistance: distance/earthRadius
+  })
+  //.populate('images')
+  .exec(function(err, posts){
+    if (!err){
+      cb(null, posts);
+    } else {
+      // we had some sort of database error
+      logger.error(err);
+      cb({err: err, clientMsg: 'Something broke, try again'}, null);
+    }
+  });
+};
 /**
  * Read all posts for a user
  * @param {object} options The options for the lookup
