d61ac1aa80fe8fce585e26ea24447cb4f8b44b3b
Avishaan <avishaan@codeHatcher.com>
2015-06-03 21:10:03 -0700

    test: specs for making sure comment model working correct #28

--- specs/integration/commentSpec.js
+++ specs/integration/commentSpec.js
@@ -2,6 +2,8 @@ var agent = require('superagent');
 var config = require("../../config.js");
 var fixture = require('./../fixtures/fixture.js');
 var User = require('../../models/user.js');
+var Comment = require('../../models/comment.js');
+var Post = require('../../models/post.js');
 //var httpMocks = require('node-mocks-http');
 
 var apiVersion = '/v1';
@@ -9,11 +11,11 @@ var URL = config.apiURI + ':' + config.expressPort + "/api" + apiVersion;
 
 // complete post for testing
 var post = {
-  _id: "12345", //TODO: remove this fake post
+  id: '1234',
   text: 'This is a post description',
   images: ['uhn43civzs6m1c9uurqvr', 'uhn43civzs6m1c9uurqvj', 'uhn43civzs6m1c9uurqvo'],
-  latitude: '1234.5',
-  longitude: '1234.5'
+  latitude: 37.796096, //San fran, google maps shows lat/lng order
+  longitude: -122.418145
 };
 
 var comment = {
@@ -60,7 +62,7 @@ describe("Comments", function() {
   //});
   it("should return all the comments for a post", function(done) {
     agent
-    .get(URL + '/posts/' + post._id + '/comments')
+    .get(URL + '/posts/' + post.id + '/comments')
     .set('Content-Type', 'application/json')
     .query({ access_token: seedUser.token })
     .end(function(res){
@@ -72,15 +74,22 @@ describe("Comments", function() {
   });
   it("should allow commenting on a post", function(done) {
     agent
-    .post(URL + '/posts/' + post._id + '/comments')
+    .post(URL + '/posts/' + post.id + '/comments')
     .set('Content-Type', 'application/json')
     .send(comment)
     .query({ access_token: seedUser.token })
     .end(function(res){
-      var body = res.body;
-      expect(body).toBeDefined();
+      var comment = res.body;
+      expect(comment).toBeDefined();
       expect(res.status).toEqual(200);
-      done();
+      expect(comment.id).toBeDefined();
+      // make sure parent of comment is the post
+      Comment
+      .findOne({_id: comment.id})
+      .exec(function(err, comment){
+        expect(comment.parent.toString()).toEqual(post.id);
+        done();
+      });
     });
   });
 });
