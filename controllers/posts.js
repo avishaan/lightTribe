4a93d600b94ccd21c3928ddf47ba382c5cac6cb3
Avishaan <avishaan@codeHatcher.com>
2015-06-03 19:13:55 -0700

    dev: remove lng/lat response to front end on post creation, #20

--- controllers/posts.js
+++ controllers/posts.js
@@ -19,8 +19,6 @@ module.exports.createPost = function (req, res, next) {
       _id: post._id,
       text: post.text,
       images: post.images,
-      longitude: post.loc.coordinates[0],
-      latitude: post.loc.coordinates[1],
       author: post.author
     });
   })
