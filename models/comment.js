d42e10500a4a5123c4ce7d25e0cb7a35ed839242
Avishaan <avishaan@codeHatcher.com>
2015-06-05 15:27:02 -0700

    dev: remove extra comment func

--- models/comment.js
+++ models/comment.js
@@ -64,41 +64,6 @@ commentSchema.statics.readComment = function(options, cb) {
     }
   });
 };
-
-/**
- * Read relevant comment based on query parameters search
- * @param {object} options The options for the lookup
- * @config {number} options.longitude longitude of the search for comments in that area using WGS84 guidelines 
- * @config {number} options.latitude latitude of the search for comments in that area using WGS84 guidelines
- * @config {number} options.distance in units of meters assuming geoJSON guidelines
- * @param {function} cb
- * @config {object} comments
- * @config {object} err Passed Error
- */
-commentSchema.statics.readCommentsBySearch = function(options, cb) {
-  // see if comment exists, if so pass error
-  var coordinates = [options.longitude, options.latitude];
-  var distance = options.distance; // km
-  var earthRadius = 6371; // km
-  Comment
-  .find({})
-  .where('loc')
-  .near({
-    center: coordinates,
-    spherical: true,
-    maxDistance: distance/earthRadius
-  })
-  //.populate('images')
-  .exec(function(err, comments){
-    if (!err){
-      cb(null, comments);
-    } else {
-      // we had some sort of database error
-      logger.error(err);
-      cb({err: err, clientMsg: 'Something broke, try again'}, null);
-    }
-  });
-};
 /**
  * Read all comments for a user
  * @param {object} options The options for the lookup
