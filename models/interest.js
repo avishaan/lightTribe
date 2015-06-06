eeeb4b6382e53ca16a54c808c82ab0f673a54843
Avishaan <avishaan@codeHatcher.com>
2015-06-06 00:00:35 -0700

    dev: add interest object for categories #36

---
+++ models/interest.js
@@ -0,0 +1,17 @@
+module.exports.Interests = [{
+  key: "yogaBikram",
+  title: "Bikram Yoga",
+  description: "Bikram Yoga Description",
+  category: "Yoga",
+}, {
+  key: "yogaVinyasa",
+  title: "Vinyasa Yoga",
+  description: "Vinyasa Yoga Description",
+  category: "Yoga",
+}, {
+  key: "meditationZen",
+  title: "Zen Meditation",
+  description: "Zen Meditation Description",
+  category: "Meditation"
+}];
+
