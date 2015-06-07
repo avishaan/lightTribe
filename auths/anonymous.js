7344e1b27496f1b3483d2beec47125d768882ebd
Avishaan <avishaan@codeHatcher.com>
2015-06-07 03:30:43 -0700

    dev: add anonymous passport auth strategy #40

---
+++ auths/anonymous.js
@@ -0,0 +1,23 @@
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user.js');
+
+module.exports = new LocalStrategy({
+  passReqToCallback: true,
+  // NOTE: we are setting the following two fields otherwise we get http error 400
+  usernameField: 'GUID',
+  passwordField: 'GUID'
+}, function(req, username, password, done) {
+  // these will be the actual fields we are interested in
+  var username = req.swagger.params.body.value.username;
+  var id = req.swagger.params.body.value.GUID;
+  User.checkAnonAuth({
+    username: username,
+    id: id
+  }, function(err, user){
+    if (!err){
+      return done(null, user);
+    } else {
+      return done(err);
+    }
+  });
+});
