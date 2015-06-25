8993f78936e5843b9a8ac20726cc379f0e2ab1c7
Avishaan <avishaan@codeHatcher.com>
2015-06-24 17:04:34 -0700

    dev: remove apn setup from debuggers

--- controllers/debuggers.js
+++ controllers/debuggers.js
@@ -4,31 +4,6 @@ var Image = require('./../models/image.js');
 var config = require('../config.js');
 var fs = require('fs');
 
-var apn = require('apn');
-var service = new apn.Connection({
-  pfx: './certs/' + config.cert.filename,
-  passphrase: config.cert.passphrase,
-  production: config.cert.production
-});
-service.on('connected', function(openSockets) {
-  console.log('Connected to Apple Notification Gateway with sockets:', openSockets);
-});
-service.on('error', function(error){
-  console.log("error: ", error);
-});
-service.on('timeout', function(){
-  console.log('timeout');
-});
-service.on('socketError', console.log);
-
-// send the notification
-var note = new apn.Notification();
-note.setAlertText("Test");
-note.badge = 1;
-service.pushNotification(note, "a591bde2 720d89d4 086beaa8 43f9b061 a18b36b4 8cd0008a 1f347a5a d844be95");
-
-
-
 module.exports.mirrorResponse = function mirrorResponse (req, res, next) {
   if (req.header('content-type') == 'application/json') {
     return res.status(500).send({
