b49798432843aaf44a4fde8834dbe5446874a4bb
Avishaan <avishaan@codeHatcher.com>
2015-07-09 16:01:11 -0400

    dev: remove register user from the auth route #61

--- controllers/auths.js
+++ controllers/auths.js
@@ -2,24 +2,6 @@ var logger = require('./../loggers/logger.js');
 var User = require('./../models/user.js');
 var config = require('../config.js');
 
-module.exports.registerUser = function registerUser (req, res, next) {
-  var username = req.swagger.params.user.value.username;
-  var password = req.swagger.params.user.value.password;
-  logger.info('registerUser');
-  User.createUser({
-    username: username,
-    password: password
-  }, function(err, user){
-    if (!err && user){
-      res.status(200).send({
-        _id: user.id
-      });
-    } else {
-      res.status(500).send(err);
-    }
-  });
-};
-
 module.exports.anonymous = function basic (req, res, next) {
   var user = req.user;
   logger.info('protected route');
