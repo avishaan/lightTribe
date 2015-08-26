56f920fc636914bb73957510fd2f2a1ffae215d6
Avishaan <avishaan@codeHatcher.com>
2015-08-25 22:48:51 -0500

    test: update spec for using conversations instead of just messages

--- specs/integration/messageSpec.js
+++ specs/integration/messageSpec.js
@@ -68,40 +68,54 @@ describe("Messages", function() {
   //});
   it("should allow user1 to message user2", function(done) {
     agent
-    .post(URL + '/messages/' + user2.id)
+    .post(URL + '/messages')
     .set('Content-Type', 'application/json')
     .query({ access_token: user1.token })
     .send({
-      text: "Hello user2, you seem cool"
+      text: "Hello user2, you seem cool",
+      recipient: user2.id
     })
     .end(function(res){
       expect(res.status).toEqual(200);
       done();
     });
   });
-  it("should allow user2 to read all of it's messages", function(done) {
+  it("should allow user2 to read all of it's conversations", function(done) {
     // first seed the message
     agent
-    .post(URL + '/messages/' + user2.id)
+    .post(URL + '/messages')
     .set('Content-Type', 'application/json')
     .query({ access_token: user1.token })
     .send({
-      text: "test message"
+      text: "Hello user2, you seem cool",
+      recipient: user2.id
     })
     .end(function(res){
       expect(res.status).toEqual(200);
-      // check the messages
+      // get a list of all the conversations for a user
       agent
-      .get(URL + '/messages/' + user2.id)
+      .get(URL + '/conversations/')
       .set('Content-Type', 'application/json')
       .query({ access_token: user2.token })
       .end(function(res){
-        var messages = res.body;
+        var conversations = res.body;
         expect(res.status).toEqual(200);
-        expect(messages.length).toEqual(1);
-        expect(messages[0].text).toEqual("test message");
-        expect(messages[0].author.username).toEqual("test2");
-        done();
+        expect(conversations.length).toEqual(1);
+        expect(conversations[0]._id).toBeDefined();
+        // save the conversation id for later
+        var conversationId = conversations[0]._id;
+        // get all the messages in the only conversation
+        agent
+        .get(URL + '/conversations/' + conversationId)
+        .set('Content-Type', 'application/json')
+        .query({ access_token: user2.token })
+        .end(function(res){
+          var messages = res.body;
+          expect(res.status).toEqual(200);
+          expect(messages.length).toEqual(1);
+          expect(messages[0]._id).toBeDefined();
+          done();
+        });
       });
     });
   });
