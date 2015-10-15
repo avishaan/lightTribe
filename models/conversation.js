259a785ae67614da8d5c2893a5892c292053eee4
Avishaan <avishaan@codeHatcher.com>
2015-10-15 01:52:23 -0700

    dev: return images for each user in conversation messages close #91

--- models/conversation.js
+++ models/conversation.js
@@ -2,8 +2,10 @@ var mongoose = require('mongoose');
 var logger = require('./../loggers/logger.js');
 var _ = require('underscore');
 var Message = require('./../models/message.js');
+var User = require('./../models/user.js');
 // get the io instance from the main app.js file so we can share with multiple files
 var io = require('./../app.js').io;
+var async = require('async');
 /*
 |-------------------------------------------------------------
 | Conversation Schema
@@ -139,7 +141,14 @@ conversationSchema.statics.readOneConversation = function(options, cb) {
   .populate('messages.author')
   .exec(function(err, conversation){
     if (!err){
-      cb(null, conversation);
+      User
+      .populate(conversation.messages, {
+        path: 'author.userImage',
+        model: 'Image'
+      }, function(err, doc){
+        console.log(err, doc);
+        cb(err, conversation);
+      });
     } else {
       // we had some sort of database error
       logger.error(err);
