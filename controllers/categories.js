904d980d3d65c1f28540b932b180520bc8ca86b7
Avishaan <avishaan@codeHatcher.com>
2015-10-15 17:57:13 -0700

    dev: return interest and event types in explicit set of routes while keeping backward compatible with existing interests route #85

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,5 +1,13 @@
 var logger = require('./../loggers/logger.js');
-var Interests = require('./../models/interests.js').Interests;
+var Interests = require('./../models/category.js').Interests;
+
+module.exports.readAllEventTypes = function (req, res, next) {
+  res.status(200).send(Interests);
+};
+
+module.exports.readAllInterestTypes = function (req, res, next) {
+  res.status(200).send(Interests);
+};
 
 module.exports.readAllInterests = function (req, res, next) {
   res.status(200).send(Interests);
--- models/category.js
+++ models/category.js
@@ -15,3 +15,19 @@ module.exports.Interests = [{
   category: "Meditation"
 }];
 
+module.exports.Events = [{
+  key: "yogaBikram",
+  title: "Bikram Yoga",
+  description: "Bikram Yoga Description",
+  category: "Yoga",
+}, {
+  key: "yogaVinyasa",
+  title: "Vinyasa Yoga",
+  description: "Vinyasa Yoga Description",
+  category: "Yoga",
+}, {
+  key: "meditationZen",
+  title: "Zen Meditation",
+  description: "Zen Meditation Description",
+  category: "Meditation"
+}];
