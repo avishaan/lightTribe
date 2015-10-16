193a5dcd554eda81bb6c44965dacda10b34252a6
Avishaan <avishaan@codeHatcher.com>
2015-10-15 18:01:16 -0700

    dev: add general categories controller #85

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,8 +1,9 @@
 var logger = require('./../loggers/logger.js');
 var Interests = require('./../models/category.js').Interests;
+var EventTypes = require('./../models/category.js').EventTypes;
 
 module.exports.readAllEventTypes = function (req, res, next) {
-  res.status(200).send(Interests);
+  res.status(200).send(EventTypes);
 };
 
 module.exports.readAllInterestTypes = function (req, res, next) {
