530b8d74d9b1d7c8a9bf99d74625c5d3fde509a1
Avishaan <avishaan@codeHatcher.com>
2015-10-15 17:38:07 -0700

    dev: refactor controller to support multiple types of categories/event types/interests for listing #85

--- specs/integration/categorySpec.js
+++ specs/integration/categorySpec.js
@@ -11,6 +11,40 @@ var URL = config.apiURI + ':' + config.expressPort + "/api" + apiVersion;
 
 var seedUser;
 
+describe("Categories", function() {
+  // delete the database before each time
+  beforeEach(function(done){
+    fixture
+    .deleteDBAsync({})
+    .then(function(dbInfo){
+      return fixture.seedUserAsync();
+    })
+    .then(function(user){
+      // save the user for later
+      seedUser = user;
+      done();
+    })
+    .caught(function(err){
+      console.log("Error: ", err);
+    });
+  });
+  it("should return all interests possible in the system", function(done) {
+    agent
+    .get(URL + '/categories/interests')
+    .set('Content-Type', 'application/json')
+    .query({ access_token: seedUser.token })
+    .end(function(res){
+      var interests = res.body;
+      expect(res.status).toEqual(200);
+      expect(interests.length).toBeDefined();
+      expect(interests[0].key).toBeDefined();
+      expect(interests[0].title).toBeDefined();
+      expect(interests[0].description).toBeDefined();
+      expect(interests[0].category).toBeDefined();
+      done();
+    });
+  });
+});
 describe("Interests", function() {
   // delete the database before each time
   beforeEach(function(done){
