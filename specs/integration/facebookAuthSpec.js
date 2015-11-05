54ccd6e79b8857a12221cd6f7c505efe53a16135
Avishaan <avishaan@codeHatcher.com>
2015-11-04 23:13:19 -0800

    dev: confirm user can change their facebook username

--- specs/integration/facebookAuthSpec.js
+++ specs/integration/facebookAuthSpec.js
@@ -42,4 +42,43 @@ describe("A user", function() {
       done();
     });
   });
+  it("should be able to change username after authenticating with facebook", function(done) {
+    var fbUser;
+    agent
+    .post(URL + '/auths/facebook')
+    //.get('http://localhost:3000/api/v1/templates')
+    .set('Content-Type', 'application/json')
+    .send({
+      access_token: user.fb_access_token
+    })
+    .end(function(res){
+      expect(res.status).toEqual(200);
+      var user = res.body;
+      fbUser = user;
+      // console.log(fbUser);
+
+      var newUsername = "changedUsername";
+
+      agent
+      .post(URL + '/users/' + fbUser.uid)
+      .set('Content-Type', 'application/json')
+      .send({
+        access_token: fbUser.token,
+        username: newUsername
+      })
+      .end(function(res){
+        var settings = res.body;
+        expect(res.status).toEqual(200);
+        // find that user and check the values now
+        User
+        .findOne({ _id: fbUser.uid })
+        .lean()
+        .exec(function(err, user){
+          expect(user.username).toEqual(newUsername);
+          // fbUser.username = user.username;
+          done();
+        });
+      });
+    });
+  });
 });
