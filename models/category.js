9787ee37a336e32dac76c2c61433d34db0ed3923
Avishaan <avishaan@codeHatcher.com>
2015-11-05 03:11:32 -0800

    dev: add new categories based on "mob app 0.1.docx" #110

--- models/category.js
+++ models/category.js
@@ -1,18 +1,68 @@
 module.exports.Interests = [{
-  key: "yogaBikram",
-  title: "Bikram Yoga",
-  description: "Bikram Yoga Description",
-  category: "Yoga",
+  key: "yoga",
+  title: "Yoga",
+  description: "Vinyasa, Bikram, and Others",
+  category: "NA",
 }, {
-  key: "yogaVinyasa",
-  title: "Vinyasa Yoga",
-  description: "Vinyasa Yoga Description",
-  category: "Yoga",
+  key: "meditation",
+  title: "Meditation",
+  description: "All types of meditation",
+  category: "NA",
 }, {
-  key: "meditationZen",
-  title: "Zen Meditation",
-  description: "Zen Meditation Description",
-  category: "Meditation"
+  key: "musicDance",
+  title: "Music & Dance",
+  description: "classical, modern, folk music and dance.",
+  category: "NA",
+}, {
+  key: "coachEmpower",
+  title: "Coach & Empowerment",
+  description: "life, money coach and empowerment",
+  category: "NA"
+}, {
+  key: "healingHealthNutrition",
+  title: "Healing & Health & Nutrition",
+  description: "all things for being healthy and well",
+  category: "NA",
+}, {
+  key: "massageSpa",
+  title: "Massage & Spa",
+  description: "relax, pamper and be pampered",
+  category: "NA",
+}, {
+  key: "tantra",
+  title: "Tantra",
+  description: "tantalizing tantra",
+  category: "NA",
+}, {
+  key: "fineArtsHandicrafts",
+  title: "Fine Arts & Handicrafts",
+  description: "let your creative side out",
+  category: "NA",
+}, {
+  key: "ecoAwarenessNatureSustainability",
+  title: "Eco Awareness, Nature & Sustainability",
+  description: "all things related to mother nature",
+  category: "NA",
+}, {
+  key: "martialArts",
+  title: "Martial Arts",
+  description: "protect you and your family",
+  category: "NA",
+}, {
+  key: "performanceArts",
+  title: "Performance Arts",
+  description: "act, sing, perform and entertain",
+  category: "NA",
+}, {
+  key: "spiritualityShamanism",
+  title: "Spirituality & Shamanism",
+  description: "explore your spirit and soul",
+  category: "NA",
+}, {
+  key: "transformationalBooksVideos",
+  title: "Transformational Books Videos & Movies",
+  description: "be a better you and share with the world",
+  category: "NA",
 }];
 
 module.exports.EventTypes = [{
