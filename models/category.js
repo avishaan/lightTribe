499f81f0db1d6a1d63d9b9b14f45516b6c6ab51a
Avishaan <avishaan@codeHatcher.com>
2015-11-05 16:51:07 -0800

    dev: add keywords to interest category response #107

--- models/category.js
+++ models/category.js
@@ -3,67 +3,79 @@ module.exports.Interests = [{
   title: "Yoga",
   description: "Vinyasa, Bikram, and Others",
   category: "NA",
-  keywords: ["yoga", "yoga2"]
+  keywords: ["yoga", "vinyasa", "bikram"]
 }, {
   key: "meditation",
   title: "Meditation",
   description: "All types of meditation",
   category: "NA",
+  keywords: ["yoga", "vinyasa", "bikram"]
 }, {
   key: "musicDance",
   title: "Music & Dance",
   description: "classical, modern, folk music and dance.",
   category: "NA",
+  keywords: ["yoga", "vinyasa", "bikram"]
 }, {
   key: "coachEmpower",
   title: "Coach & Empowerment",
   description: "life, money coach and empowerment",
-  category: "NA"
+  category: "NA",
+  keywords: ["yoga", "vinyasa", "bikram"]
 }, {
   key: "healingHealthNutrition",
   title: "Healing & Health & Nutrition",
   description: "all things for being healthy and well",
   category: "NA",
+  keywords: ["yoga", "vinyasa", "bikram"]
 }, {
   key: "massageSpa",
   title: "Massage & Spa",
   description: "relax, pamper and be pampered",
   category: "NA",
+  keywords: ["yoga", "vinyasa", "bikram"]
 }, {
   key: "tantra",
   title: "Tantra",
   description: "tantalizing tantra",
   category: "NA",
+  keywords: ["yoga", "vinyasa", "bikram"]
 }, {
   key: "fineArtsHandicrafts",
   title: "Fine Arts & Handicrafts",
   description: "let your creative side out",
   category: "NA",
+  keywords: ["yoga", "vinyasa", "bikram"]
 }, {
   key: "ecoAwarenessNatureSustainability",
   title: "Eco Awareness, Nature & Sustainability",
   description: "all things related to mother nature",
   category: "NA",
+  keywords: ["yoga", "vinyasa", "bikram"]
 }, {
   key: "martialArts",
   title: "Martial Arts",
   description: "protect you and your family",
   category: "NA",
+  keywords: ["yoga", "vinyasa", "bikram"]
 }, {
   key: "performanceArts",
   title: "Performance Arts",
   description: "act, sing, perform and entertain",
   category: "NA",
+  keywords: ["yoga", "vinyasa", "bikram"]
 }, {
   key: "spiritualityShamanism",
   title: "Spirituality & Shamanism",
   description: "explore your spirit and soul",
   category: "NA",
+  keywords: ["yoga", "vinyasa", "bikram"]
 }, {
   key: "transformationalBooksVideos",
   title: "Transformational Books Videos & Movies",
   description: "be a better you and share with the world",
   category: "NA",
+  keywords: ["yoga", "vinyasa", "bikram"]
 }];
 
 module.exports.EventTypes = [{
